7a542437eab2bab94ab681a05deba26df8dba62b
Crowdbotics <engineering@crowdbotics.com>
2023-06-22 23:53:48 +0000

    Initial commit.

---
+++ backend/little_smoke_dev_93921/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for little_smoke_dev_93921 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_smoke_dev_93921.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "little-smoke-dev-93921",
+  url: "https://little-smoke-dev-93921.botics.co",
+  api: "https://little-smoke-dev-93921.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
